9387181cfe722f8e759583ff862959735c948dd8
Hector Gonzalez <Heco_gs@hotmail.com>
2024-06-30 11:44:53 -0600

    add commmit

---
+++ src/main/java/com/alurachallenge/forohub/domain/topic/Topic.java
@@ -0,0 +1,4 @@
+package com.alurachallenge.forohub.domain.topic;
+
+public class Topic {
+}
---
+++ src/main/java/com/alurachallenge/forohub/domain/topic/User.java
@@ -0,0 +1,12 @@
+package com.alurachallenge.forohub.domain.topic;
+
+public class User {
+
+    private Long id;
+    private String title;
+    private String mensage;
+    private String date;
+    private Boolean status;
+    private String author;
+    private String course;
+}
